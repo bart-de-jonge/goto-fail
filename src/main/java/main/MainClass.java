4b62c2977f001092f2c1468249ab9f89c27073b2
Martijn Janssen <martijn9612@gmail.com>
2016-04-18 10:10:10 +0200

    Add lombok plugin to dependencies & example

--- src/main/java/main/MainClass.java
+++ src/main/java/main/MainClass.java
@@ -3,6 +3,7 @@
  */
 package main;
 
+<<<<<<< Updated upstream
 /**
  * @author Bart
  *
@@ -18,4 +19,22 @@ public class MainClass {
 
 	}
 
+=======
+import lombok.Getter;
+import lombok.Setter;
+
+public class MainClass {
+    @Getter
+    @Setter
+    public static int counter;
+
+    /**
+     * @param args - the main arguments.
+     */
+    public static void main(String[] args) {
+        setCounter(5);
+        System.out.println(getCounter());
+    System.out.println("Welcome to the main class of goto-fail;");
+    }
+>>>>>>> Stashed changes
 }
