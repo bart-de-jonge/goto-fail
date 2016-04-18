4dc8384bcdb693c39d082925a215049ed17fe52c
Menno Oudshoorn <m.j.j.oudshoorn@student.tudelft.nl>
2016-04-18 10:54:52 +0200

    Fixed checkstyle errors

--- src/main/java/main/MainClass.java
+++ src/main/java/main/MainClass.java
@@ -5,6 +5,7 @@ public class MainClass {
      * @param args - the main arguments.
      */
     public static void main(String[] args) {
-    System.out.println("Welcom to the main class of goto-fail;");
+        System.out.println("Welcom to the main class of goto-fail;");
     }
 }
+
