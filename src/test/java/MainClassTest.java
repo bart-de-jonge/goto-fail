534473366fe55382b398defa7dedc7dddfac11bd
Bart de Jonge <B.A.P.deJonge@student.tudelft.nl>
2016-04-18 23:12:17 +0200

    removed all nonsense for testing configurations --> no warnings

--- src/test/java/MainClassTest.java
+++ src/test/java/MainClassTest.java
@@ -1,17 +1,11 @@
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
-import org.junit.Before;
 import org.junit.Test;
 
 public class MainClassTest {
     
-    private int x = 5;
-    
     @Test
     public void testGetter() {
-        MainClass.getCounter();
         assertTrue(true);
     }
 
