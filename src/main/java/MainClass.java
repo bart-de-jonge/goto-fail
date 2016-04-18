7a1fa11503b80d20429ac1d842ec231d12761c99
Menno Oudshoorn <m.j.j.oudshoorn@student.tudelft.nl>
2016-04-18 15:28:06 +0200

    Testing travis re-initiated

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -11,5 +11,6 @@ public class MainClass {
     public static void main(String[] args) {
         System.out.println(getCounter());
         System.out.println("Welcome to the main class of goto-fail;");
+        System.out.println("For commit diff");
     }
 }
