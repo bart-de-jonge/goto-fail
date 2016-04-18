0524e0a4896e9c0e86f06fef337da5b7bdca7e41
Menno Oudshoorn <m.j.j.oudshoorn@student.tudelft.nl>
2016-04-18 12:47:47 +0200

    Fix checkstyle magic numbers

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -5,13 +5,13 @@ public class MainClass {
     @Getter @Setter
     public static int counter;
     
-    private int hoi = 7;
+    
 
     /**
      * @param args - the main arguments.
      */
     public static void main(String[] args) {
-        setCounter(5);
+        
         System.out.println(getCounter());
         System.out.println("Welcome to the main class of goto-fail;");
         
