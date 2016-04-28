31b1e3520631512a77d17a3cba7269d88a7fb7e6
Alexander Geenen <geenen124@gmail.com>
2016-04-28 23:17:20 +0200

    Create Dummy Objects & AddCameraShot TimelineController Method

--- src/main/java/control/TimelineController.java
+++ src/main/java/control/TimelineController.java
@@ -1,8 +1,15 @@
 package control;
 
+import data.Camera;
+import data.CameraTimeline;
+import data.CameraType;
+import data.ScriptingProject;
 import gui.CameraShotBlock;
 import gui.TimelinesGridPane;
 import gui.RootPane;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Class that controls the timeline.
@@ -13,6 +20,14 @@ public class TimelineController {
     private TimelinesGridPane timelinePane;
     private RootPane rootPane;
 
+    private List<CameraTimeline> cameraTimelines;
+
+    // Placeholder camera type until GUI allows personalized entry
+    private final CameraType defType = new CameraType("AW-HE130 HD PTZ", "It's an IP Camera", 0.5);
+
+    // Placeholder project in lieu of XML loading
+    private final ScriptingProject scriptingProject = new ScriptingProject("BOSS Project", 1.0);
+
     /**
      * Constructor.
      * @param timelinePane Timeline view to be controlled.
@@ -21,5 +36,34 @@ public class TimelineController {
     public TimelineController(TimelinesGridPane timelinePane, RootPane rootPane) {
         this.timelinePane = timelinePane;
         this.rootPane = rootPane;
+
+        initializeCameraTimelines();
+    }
+
+    /**
+     * Add a camera shot to the corresponding timeline
+     * @param cameraIndex Index of the camera track.
+     * @param name Name of the shot.
+     * @param description Shot description.
+     * @param startCount Start count.
+     * @param endCount End count.
+     */
+    public void addCameraShot(int cameraIndex, String name, String description,
+                              int startCount, int endCount) {
+        this.cameraTimelines.get(cameraIndex).addShot(name, description, startCount, endCount);
+    }
+
+    /**
+     * Create camera timelines based on GUI. (Anti-pattern)
+     * TODO: Replace this with proper XML based project creation
+     */
+    private void initializeCameraTimelines() {
+        cameraTimelines = new ArrayList<>();
+
+        for (int i = 0; i < this.timelinePane.getNumberOfTimelines(); i++) {
+            Camera defCam = new Camera("IP Cam " + i, "", defType);
+            CameraTimeline timelineN = new CameraTimeline(defCam, "", scriptingProject);
+            cameraTimelines.add(timelineN);
+        }
     }
 }
