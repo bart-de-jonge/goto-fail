f29a4b59538b28b4b205b6965fea18903d6b2ed9
Martijn Janssen <martijn9612@gmail.com>
2016-05-02 13:18:57 +0200

    Add logging to Timeline

--- src/main/java/data/Timeline.java
+++ src/main/java/data/Timeline.java
@@ -2,12 +2,17 @@ package data;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 /**
  * Created by Bart.
  * Abstract class for timelines.
  */
 public abstract class Timeline {
+
+    // Logger
+    private static Logger logger = LogManager.getLogger();
 
     // Description of this Timeline.
     @Getter
@@ -23,6 +28,7 @@ public abstract class Timeline {
      * @param project the project that contains this timeline
      */
     public Timeline(String description, ScriptingProject project) {
+        logger.info("Creating new Timeline.");
         this.description = description;
         this.project = project;
     }
@@ -36,11 +42,14 @@ public abstract class Timeline {
      * @return true when the two shots are overlapping, false if not
      */
     public boolean checkOverlap(Shot s1, Shot s2, double seconds) {
+        logger.info("Checking overlap between '{}' and '{}'", s1.getName(), s2.getName());
         if (s1.areOverlapping(s2, project.secondsToCounts(seconds))) {
+            logger.info("Shots overlap.");
             s1.setOverlapping(true);
             s2.setOverlapping(true);
             return true;
         }
+        logger.info("Shots don't overlap.");
         return false;
     }
 
