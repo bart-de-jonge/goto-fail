15928954ef2384556b99e4ea82dab7e598b8c5f4
Mark van de Ruit <markvanderuit@gmail.com>
2016-05-13 13:44:41 +0200

    Started work on the detail view.

--- src/main/java/gui/headerarea/DetailView.java
+++ src/main/java/gui/headerarea/DetailView.java
@@ -1,6 +1,8 @@
 package gui.headerarea;
 
+import gui.misc.TweakingHelper;
 import gui.styling.StyledTextfield;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.ColumnConstraints;
@@ -13,28 +15,29 @@ import lombok.Getter;
 /**
  * Created by Bart.
  */
-public class DetailView extends VBox {
+public class DetailView extends HBox {
 
-    private GridPane grid;
+    //private GridPane grid;
+
+    private String style = "-fx-background-color: "
+            + TweakingHelper.STRING_TERTIARY + ";"
+            + "-fx-min-height: 50;";
 
     private static final String defaultEmptyString = "";
     private static final String defaultEmptyNumber = "0";
 
-    private int numberOfColumns = 3;
-    private int numberOfRows = 2;
+    private int numberOfColumns = 4;
+    private int numberOfRows = 1;
 
     private double columnWidth = 200;
     private double rowHeight = 50;
 
     @Getter
     private StyledTextfield nameField;
-
     @Getter
     private StyledTextfield descriptionField;
-
     @Getter
     private DoubleTextField beginCountField;
-
     @Getter
     private DoubleTextField endCountField;
 
@@ -42,18 +45,18 @@ public class DetailView extends VBox {
      * Constructor.
      */
     public DetailView() {
-        this.grid = new GridPane();
-        this.getChildren().addAll(grid);
-
-        for (int i = 0; i < numberOfColumns; i++) {
-            grid.getColumnConstraints().add(new ColumnConstraints(columnWidth));
-        }
-        for (int i = 0; i < numberOfRows; i++) {
-            grid.getRowConstraints().add(new RowConstraints(rowHeight));
-        }
-
-        grid.setGridLinesVisible(true);
-
+//        this.grid = new GridPane();
+//        this.getChildren().addAll(grid);
+//
+//        for (int i = 0; i < numberOfColumns - 1; i++) {
+//            grid.getColumnConstraints().add(new ColumnConstraints(columnWidth));
+//        }
+//        for (int i = 0; i < numberOfRows; i++) {
+//            grid.getRowConstraints().add(new RowConstraints(rowHeight));
+//        }
+//
+//        grid.setGridLinesVisible(true);
+        this.setStyle(style);
         initName();
         initDescription();
         initBeginCount();
@@ -109,30 +112,36 @@ public class DetailView extends VBox {
      * Init the begincount part of the detailview.
      */
     private  void initBeginCount() {
-        beginCountField = new DoubleTextField();
-        beginCountField.setText("123");
-        beginCountField.setPrefWidth(50);
-
+        beginCountField = new DoubleTextField("123");
+        beginCountField.setBorderColor(TweakingHelper.COLOR_PRIMARY);
+        beginCountField.setTextColor(TweakingHelper.COLOR_PRIMARY);
+        beginCountField.setTextActiveColor(TweakingHelper.COLOR_SECONDARY);
+        beginCountField.setFillActiveColor(TweakingHelper.COLOR_TERTIARY);
+        beginCountField.setAlignment(Pos.CENTER);
+        //beginCountField.setPrefWidth(50);
         HBox startCountBox = new HBox();
-
         Label specifierLabel = new Label("Start Count:");
         startCountBox.getChildren().addAll(specifierLabel, beginCountField);
-        grid.add(startCountBox, 1, 0);
+        this.getChildren().add(startCountBox);
+        //grid.add(startCountBox, 1, 0);
     }
 
     /**
      * Init the endcount part of the detailview.
      */
     private  void initEndCount() {
-        endCountField = new DoubleTextField();
-        endCountField.setText("123");
-        endCountField.setPrefWidth(50);
-
+        endCountField = new DoubleTextField("123");
+        endCountField.setBorderColor(TweakingHelper.COLOR_PRIMARY);
+        endCountField.setTextColor(TweakingHelper.COLOR_PRIMARY);
+        endCountField.setTextActiveColor(TweakingHelper.COLOR_SECONDARY);
+        endCountField.setFillActiveColor(TweakingHelper.COLOR_TERTIARY);
+        endCountField.setAlignment(Pos.CENTER);
+        //endCountField.setPrefWidth(50);
         HBox endCountBox = new HBox();
-
         Label specifierLabel = new Label("End Count:");
         endCountBox.getChildren().addAll(specifierLabel, endCountField);
-        grid.add(endCountBox, 2, 0);
+        this.getChildren().add(endCountBox);
+        //grid.add(endCountBox, 2, 0);
     }
 
     /**
@@ -140,11 +149,16 @@ public class DetailView extends VBox {
      */
     private void initName() {
         nameField = new StyledTextfield("Placeholder");
+        nameField.setBorderColor(TweakingHelper.COLOR_PRIMARY);
+        nameField.setTextColor(TweakingHelper.COLOR_PRIMARY);
+        nameField.setTextActiveColor(TweakingHelper.COLOR_SECONDARY);
+        nameField.setFillActiveColor(TweakingHelper.COLOR_TERTIARY);
+        nameField.setAlignment(Pos.CENTER);
         HBox nameBox = new HBox();
-
         Label specifierLabel = new Label("Name:");
         nameBox.getChildren().addAll(specifierLabel, nameField);
-        grid.add(nameBox, 0, 0);
+        this.getChildren().add(nameBox);
+        //grid.add(nameBox, 0, 0);
     }
 
     /**
@@ -152,14 +166,18 @@ public class DetailView extends VBox {
      */
     private void initDescription() {
         descriptionField = new StyledTextfield("");
-        descriptionField.setPrefWidth(300);
-
+        descriptionField.setBorderColor(TweakingHelper.COLOR_PRIMARY);
+        descriptionField.setTextColor(TweakingHelper.COLOR_PRIMARY);
+        descriptionField.setTextActiveColor(TweakingHelper.COLOR_SECONDARY);
+        descriptionField.setFillActiveColor(TweakingHelper.COLOR_TERTIARY);
+        descriptionField.setAlignment(Pos.CENTER);
+        //descriptionField.setPrefWidth(300);
         HBox descriptionBox = new HBox();
-
         Label specifierLabel = new Label("Description:");
         descriptionBox.getChildren().addAll(specifierLabel, descriptionField);
-        grid.add(descriptionBox, 0, 1);
-        grid.setColumnSpan(descriptionBox, 2);
+        this.getChildren().add(descriptionBox);
+        //grid.add(descriptionBox, 3, 0);
+        //grid.setColumnSpan(descriptionBox, 2);
     }
 
     /**
