5e17de225e478030b142f727c86f4d260dc97fa5
Bart de Jonge <B.A.P.deJonge@student.tudelft.nl>
2016-05-17 12:05:43 +0200

    added basics of tmieline page

--- src/main/webserver/routes/router.js
+++ src/main/webserver/routes/router.js
@@ -2,8 +2,10 @@
  * Created by Bart on 13/05/2016.
  */
 var index = require('./index');
+var timeline = require('./timeline');
 
 
 module.exports.addRoutes = function(app) {
     app.use('/', index);
+    app.use('/timeline', timeline);
 }
--- src/main/webserver/routes/timeline.js
+++ src/main/webserver/routes/timeline.js
@@ -1,9 +1,12 @@
+/**
+ * Created by Bart on 17/05/2016.
+ */
 var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index');
+    res.render('timeline');
 });
 
 module.exports = router;
