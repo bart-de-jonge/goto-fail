508b0fec2a18482df2d001c09bace5937279bc36
Alexander Geenen <geenen124@gmail.com>
2016-05-17 22:06:51 +0200

    Finish initial eslint fixes & add favicon

--- src/main/webserver/routes/router.js
+++ src/main/webserver/routes/router.js
@@ -1,9 +1,5 @@
-/**
- * Created by Bart on 13/05/2016.
- */
-var index = require('./index');
+import index from "./index";
 
-
-module.exports.addRoutes = function(app) {
-    app.use('/', index);
-}
+module.exports.addRoutes = (app) => {
+    app.use("/", index);
+};
