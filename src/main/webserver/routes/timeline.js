a1c5028ff0a0f5e34b33345f1b872e1e62bc259f
Bart de Jonge <B.A.P.deJonge@student.tudelft.nl>
2016-05-17 22:51:49 +0200

    made table containing all the counts in the current list of timelineblocks

--- src/main/webserver/routes/timeline.js
+++ src/main/webserver/routes/timeline.js
@@ -3,10 +3,35 @@
  */
 var express = require('express');
 var router = express.Router();
+var TimelineBlock = require('./../objects/timelineBlock');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('timeline');
+
+    // Dummy array here
+    // Todo: replace with real things,
+    // note: the array must be ordered or be sorted here
+    var timelineBlocks = [];
+    timelineBlocks.push(new TimelineBlock(2, 4));
+    timelineBlocks.push(new TimelineBlock(7, 12));
+
+
+    // Calculate minimum and maximum counts
+    var minCount = 0;
+    var maxCount = 0;
+
+    if (timelineBlocks.length > 0) {
+        minCount = timelineBlocks[0].beginCount;
+        maxCount = timelineBlocks[timelineBlocks.length - 1].endCount;
+    }
+
+    console.log(minCount);
+    console.log(maxCount);
+
+    res.render('timeline', {
+        timelineBlocks: timelineBlocks,
+        minCount: minCount,
+        maxCount: maxCount});
 });
 
 module.exports = router;
