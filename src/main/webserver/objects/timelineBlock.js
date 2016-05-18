ffff512c8dc725323bcaa33b03f9d485ba95f10b
Bart de Jonge <B.A.P.deJonge@student.tudelft.nl>
2016-05-18 11:08:36 +0200

    fixed eslint errors

--- src/main/webserver/objects/timelineBlock.js
+++ src/main/webserver/objects/timelineBlock.js
@@ -1,7 +1,7 @@
-module.exports = function TimelineBlock (beginCount, endCount) {
+module.exports = function TimelineBlock(beginCount, endCount) {
     this.beginCount = beginCount;
     this.endCount = endCount;
-    this.getLength = function() {
+    this.getLength = function getLength() {
         return this.endCount - this.beginCount;
     };
 };
--- src/main/webserver/routes/timeline.js
+++ src/main/webserver/routes/timeline.js
@@ -1,38 +1,35 @@
 /**
  * Created by Bart on 17/05/2016.
  */
-var express = require('express');
-var router = express.Router();
-var TimelineBlock = require('./../objects/timelineBlock');
+
+import express from "express";
+import TimelineBlock from "./../objects/timelineBlock";
+const router = new express.Router();
 
 /* GET home page. */
-router.get('/', function (req, res) {
-
+router.get("/", (req, res) => {
     // Dummy array here
     // Todo: replace with real things,
     // note: the array must be ordered or be sorted here
-    var timelineBlocks = [];
-    timelineBlocks.push(new TimelineBlock(1, 5));
+    const timelineBlocks = [];
+    timelineBlocks.push(new TimelineBlock(3, 5));
     timelineBlocks.push(new TimelineBlock(7, 12));
     timelineBlocks.push(new TimelineBlock(15, 22));
 
 
     // Calculate minimum and maximum counts
-    var minCount = 0;
-    var maxCount = 0;
+    let minCount = 0;
+    let maxCount = 0;
 
     if (timelineBlocks.length > 0) {
         minCount = timelineBlocks[0].beginCount;
         maxCount = timelineBlocks[timelineBlocks.length - 1].endCount;
     }
 
-    console.log(minCount);
-    console.log(maxCount);
-
-    res.render('timeline', {
-        timelineBlocks: timelineBlocks,
-        minCount: minCount,
-        maxCount: maxCount});
+    res.render("timeline", {
+        timelineBlocks,
+        minCount,
+        maxCount });
 });
 
 module.exports = router;
