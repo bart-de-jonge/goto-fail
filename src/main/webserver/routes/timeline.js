c7545e50d8732480d7d1b972d432b3f42d157c06
Bart de Jonge <B.A.P.deJonge@student.tudelft.nl>
2016-05-19 12:38:44 +0200

    added support for different cameratimelines

--- src/main/webserver/routes/timeline.js
+++ src/main/webserver/routes/timeline.js
@@ -18,28 +18,49 @@ router.get("/", (req, res) => {
 
     fs.readFile(`${__dirname}/../test_project.scp`, (err, data) => {
         parser.parseString(data, (err, result) => {
-            const cameraTimelines = result.scriptingProject["camera-centerarea"][0].cameraTimeline;
-            const firstTimeline = cameraTimelines[0].shotList[0];
+            const cameraTimelinesXML = result.scriptingProject["camera-centerarea"][0].cameraTimeline;
+            const firstTimelineXML = cameraTimelinesXML[0].shotList[0];
+            const secondTimelineXML = cameraTimelinesXML[1].shotList[0];
 
             const cameraTimeline = new CameraTimeline("dummyName", "dummyDescription");
-            firstTimeline.shot.forEach(shot => {
+            firstTimelineXML.shot.forEach(shot => {
                 cameraTimeline.addCameraShot(new CameraShot(shot.beginCount[0],
                     shot.endCount[0], shot.name[0], shot.description[0]));
             });
+            const cameraTimeline2 = new CameraTimeline("dummyName2", "dummyDescription2");
+            secondTimelineXML.shot.forEach(shot => {
+                cameraTimeline2.addCameraShot(new CameraShot(shot.beginCount[0],
+                    shot.endCount[0], shot.name[0], shot.description[0]));
+            });
+
+            const cameraTimelines = new Array();
+            cameraTimelines.push(cameraTimeline);
+            cameraTimelines.push(cameraTimeline2);
 
             // Calculate minimum and maximum counts
             let minCount = 0;
             let maxCount = 0;
 
+            const flattenedTimeline = new Array();
+            cameraTimeline.getCameraShots().forEach(shot => {
+                flattenedTimeline.push(shot);
+            });
+            cameraTimeline2.getCameraShots().forEach(shot => {
+                flattenedTimeline.push(shot);
+            })
+
             if (cameraTimeline.getCameraShots().length > 0) {
                 minCount = Number(cameraTimeline.getCameraShots()[0].beginCount);
-                cameraTimeline.getCameraShots().forEach(shot => {
+                flattenedTimeline.forEach(shot => {
+                    console.log(shot.beginCount);
+                    console.log(minCount);
                     if (shot.beginCount < minCount) {
                         minCount = Number(shot.beginCount);
+                        console.log(minCount);
                     }
                 });
                 maxCount = Number(cameraTimeline.getCameraShots()[0].endCount);
-                cameraTimeline.getCameraShots().forEach(shot => {
+                flattenedTimeline.forEach(shot => {
                     if (shot.endCount > maxCount) {
                         maxCount = Number(shot.endCount);
                     }
@@ -47,7 +68,7 @@ router.get("/", (req, res) => {
             }
 
             res.render("timeline", {
-                cameraTimeline,
+                cameraTimelines,
                 minCount,
                 maxCount });
         });
