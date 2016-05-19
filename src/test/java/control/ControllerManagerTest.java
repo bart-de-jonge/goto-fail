6b7ca8a1cf52d2b505819b2e529f13f9645d2417
Alexander Geenen <geenen124@gmail.com>
2016-05-19 21:31:00 +0200

    Further Test ControllerManager

--- src/test/java/control/ControllerManagerTest.java
+++ src/test/java/control/ControllerManagerTest.java
@@ -1,11 +1,15 @@
 package control;
 
+import data.ScriptingProject;
 import gui.root.RootPane;
+import javafx.stage.Stage;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
 import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 /**
  * @author alex
@@ -20,13 +24,36 @@ public class ControllerManagerTest {
         TimelineController timelineController = Mockito.mock(TimelineController.class);
         DetailViewController detailViewController = Mockito.mock(DetailViewController.class);
         ToolViewController toolViewController = Mockito.mock(ToolViewController.class);
+        DirectorTimelineController directorTimelineController = Mockito.mock(DirectorTimelineController.class);
+        ProjectController projectController = Mockito.mock(ProjectController.class);
 
         controllerManager = new ControllerManager(rootMock, timelineController,
-                                                  detailViewController, toolViewController);
+                                                  detailViewController, toolViewController,
+                                                  directorTimelineController, projectController);
     }
 
     @Test
     public void getTimelineControllerTest() {
         assertNotNull(controllerManager.getTimelineControl());
+    }
+
+    @Test
+    public void getDirectorTimelineControllerTest() {
+        assertNotNull(controllerManager.getDirectorTimelineControl());
+    }
+
+    @Test
+    public void getToolViewControllerTest() {
+        assertNotNull(controllerManager.getToolViewController());
+    }
+
+    @Test
+    public void getDetailViewControllerTest() {
+        assertNotNull(controllerManager.getDetailViewController());
+    }
+
+    @Test
+    public void getProjectControllerTest() {
+        assertNotNull(controllerManager.getProjectController());
     }
 }
