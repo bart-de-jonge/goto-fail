9f585d84b8d7ae3eb186e3b818ad2aa7a5b2aba7
Bart de Jonge <B.A.P.deJonge@student.tudelft.nl>
2016-05-20 13:05:52 +0200

    fixed eslint error

--- src/main/webserver/routes/upload-scp.js
+++ src/main/webserver/routes/upload-scp.js
@@ -5,15 +5,19 @@ import multipart from "connect-multiparty";
 const multipartMiddleware = multipart;
 
 router.post("/", multipartMiddleware, (req, res) => {
-    const newPath = __dirname + "/../project-scp-fies/project.scp";
-    fs.rename(req.files.project.path, newPath, (err) => {
-        if (err) {
-            res.json({ succes: false, message:
-                "Some error occurred, please try again later!" });
-        } else {
-            res.json({ succes: true, message: "Project successfully uploaden!" });
-        }
-    });
+    const newPath = `${__dirname}/../project-scp-fies/project.scp`;
+    if (req.files.project.name.endsWith(".scp")) {
+        fs.rename(req.files.project.path, newPath, (err) => {
+            if (err) {
+                res.json({
+                    succes: false, message: "Some error occurred, please try again later!" });
+            } else {
+                res.json({ succes: true, message: "Project successfully uploaden!" });
+            }
+        });
+    } else {
+        res.json({ succes: false, message: "Please upload a .scp file." });
+    }
 });
 
 module.exports = router;
