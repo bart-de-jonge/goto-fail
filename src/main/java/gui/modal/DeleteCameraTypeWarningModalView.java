66eb67178f1b80d5be1ac6316f75c9fb731257e4
Mark van de Ruit <markvanderuit@gmail.com>
2016-05-20 13:29:30 +0200

    Added styling to DeleteCameraTypeWarningModalView. Fixed issue where the modal view could not display enough content. Fixed issue where it asked for camera deletion instead of camera type deletion.

--- src/main/java/gui/modal/DeleteCameraTypeWarningModalView.java
+++ src/main/java/gui/modal/DeleteCameraTypeWarningModalView.java
@@ -3,29 +3,64 @@ package gui.modal;
 import java.util.List;
 
 import data.Camera;
+import gui.misc.TweakingHelper;
 import gui.root.RootPane;
+import gui.styling.StyledButton;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import lombok.Getter;
 
 public class DeleteCameraTypeWarningModalView extends ModalView {
     
+
+
+    /*
+     * Tweakable styling variables.
+     */
+
+    // width and height of the screen.
+    private static final int width = 550;
+    private static final int height = 350;
+
+    // variables for spacing
+    private static final int topAreaHeight = 80;
+    private static final int bottomAreaHeight = 60;
+
+    // simple background styles of the three main areas.
+    private String topStyle = "-fx-background-color: " + TweakingHelper.STRING_PRIMARY + ";"
+            + "-fx-text-fill: white; -fx-font-size: 20;"
+            + "-fx-font-family: helvetica neue; -fx-font-weight: lighter;"
+            + "-fx-border-width: 0 0 10 0;"
+            + "-fx-border-color: " + TweakingHelper.STRING_SECONDARY + ";";
+    private String bottomStyle = "-fx-background-color: "
+            + TweakingHelper.STRING_PRIMARY + ";";
+
+    // variables for the buttons
+    private int buttonSpacing = 20;
+
+    // variables for the title label
+    private static final int titlelabelOffsetFromLeft = 20;
+
+    /*
+     * Other variables
+     */
+
     @Getter
-    private Button confirmButton;
+    private StyledButton confirmButton;
     @Getter
-    private Button cancelButton;
+    private StyledButton cancelButton;
     @Getter
     private VBox viewPane;
     @Getter
     private Label titleLabel;
     @Getter
     private List<Camera> camerasToShow;
-    
-    private static final int width = 300;
-    private static final int height = 300;
-    
+
     /**
      * Constructor that uses default width and height.
      * @param rootPane the rootPane for this modal.
@@ -58,7 +93,7 @@ public class DeleteCameraTypeWarningModalView extends ModalView {
         getModalStage().setMinHeight(height);
         getModalStage().setMinWidth(width);
         
-        this.viewPane = new VBox(20);
+        this.viewPane = new VBox();
         
         initTitleLabel();
         initMessage();
@@ -72,7 +107,16 @@ public class DeleteCameraTypeWarningModalView extends ModalView {
      * Initialize the title label.
      */
     private void initTitleLabel() {
-        titleLabel = new Label("Are you sure you want to delete this camera?");
+        titleLabel = new Label("Are you sure you want to delete this camera type?\n"
+            + "The following cameras will be deleted!");
+        titleLabel.setStyle(topStyle);
+        titleLabel.setAlignment(Pos.CENTER);
+        titleLabel.setPadding(new Insets(0, titlelabelOffsetFromLeft,
+                0, titlelabelOffsetFromLeft));
+        titleLabel.setPrefWidth(TweakingHelper.GENERAL_SIZE);
+        titleLabel.setMinHeight(topAreaHeight);
+        titleLabel.setPrefHeight(topAreaHeight);
+        titleLabel.setMaxHeight(topAreaHeight);
         this.viewPane.getChildren().add(titleLabel);
     }
     
@@ -80,27 +124,59 @@ public class DeleteCameraTypeWarningModalView extends ModalView {
      * Initialize the message.
      */
     private void initMessage() {
+        // Scrollpane to handle too-large number of cameras
+        ScrollPane scrollPane = new ScrollPane();
+        scrollPane.setPrefHeight(TweakingHelper.GENERAL_SIZE);
+        scrollPane.setHbarPolicy(ScrollPane.ScrollBarPolicy.NEVER);
+        this.viewPane.getChildren().add(scrollPane);
+
+        // VBox to make list of cameras.
         VBox messageArea = new VBox();
-        Label explanationLabel = new Label("The following cameras will be invalid and deleted:");
-        messageArea.getChildren().add(explanationLabel);
-        camerasToShow.forEach(e -> {
-                HBox cameraBox = new HBox();
-                cameraBox.getChildren().addAll(
-                        new Label(e.getName()), new Label(" - "), new Label(e.getDescription()));
-                messageArea.getChildren().add(cameraBox);
-            });
-        this.viewPane.getChildren().add(messageArea);
+        messageArea.maxWidthProperty().bind(scrollPane.widthProperty());
+        scrollPane.setContent(messageArea);
+
+        // Run through cameras, adding them to VBox, differing styling depending on
+        // odd or even camera-number. Gives list-like effect.
+        boolean even = false;
+        for (int i = 0; i < camerasToShow.size(); i++) {
+            HBox cameraBox = new HBox();
+            cameraBox.getChildren().addAll(
+                    new Label(camerasToShow.get(i).getName()),
+                    new Label(" - "),
+                    new Label(camerasToShow.get(i).getDescription())
+            );
+
+            if (even) {
+                cameraBox.setStyle("-fx-background-color: rgb(245,245,245);");
+            }
+
+            cameraBox.setPrefWidth(TweakingHelper.GENERAL_SIZE);
+            cameraBox.setPadding(new Insets(3, 0, 3, 6));
+            messageArea.getChildren().add(cameraBox);
+
+            even = !even;
+        }
     }
     
     /**
      * Initialize the buttons.
      */
     private void initButtons() {
-        HBox buttonBox = new HBox();
-        confirmButton = createButton("Confirm delete", true);
-        cancelButton = createButton("Cancel", true);
-        buttonBox.getChildren().addAll(confirmButton, cancelButton);
-        this.viewPane.getChildren().add(buttonBox);
+        HBox content = new HBox();
+        content.setSpacing(buttonSpacing);
+        content.setAlignment(Pos.CENTER_LEFT);
+        content.setMinHeight(bottomAreaHeight);
+        content.setPrefHeight(bottomAreaHeight);
+        content.setMaxHeight(bottomAreaHeight);
+        content.setStyle(bottomStyle);
+        content.setPrefHeight(TweakingHelper.GENERAL_SIZE);
+        content.setPadding(new Insets(0, 0, 0, titlelabelOffsetFromLeft));
+        this.viewPane.getChildren().add(content);
+
+        confirmButton = createButton("Confirm", false);
+        cancelButton = createButton("Cancel", false);
+
+        content.getChildren().addAll(confirmButton, cancelButton);
     }
 
 }
