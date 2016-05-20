57864679464f36a8df325ea9f702ea386e13549a
Bart de Jonge <B.A.P.deJonge@student.tudelft.nl>
2016-05-20 10:29:20 +0200

    added auto loading of all tmielines in the xml file

--- src/main/webserver/routes/timeline.js
+++ src/main/webserver/routes/timeline.js
@@ -9,58 +9,42 @@ const parser = new xml2js.Parser();
 
 /* GET home page. */
 router.get("/", (req, res) => {
-    // Dummy array here
-    // Todo: replace with real things,
-    // note: the array must be ordered or be sorted here
-    // timelineBlocks.push(new CameraShot(3, 5));
-    // timelineBlocks.push(new CameraShot(7, 12));
-    // timelineBlocks.push(new CameraShot(15, 22));
 
+    // Dummyfile Todo: replace with dynamic
     fs.readFile(`${__dirname}/../test_project.scp`, (err, data) => {
         parser.parseString(data, (err, result) => {
             const cameraTimelinesXML = result.scriptingProject["camera-centerarea"][0].cameraTimeline;
-            const firstTimelineXML = cameraTimelinesXML[0].shotList[0];
-            const secondTimelineXML = cameraTimelinesXML[1].shotList[0];
-
-            const cameraTimeline = new CameraTimeline("dummyName", "dummyDescription");
-            firstTimelineXML.shot.forEach(shot => {
-                cameraTimeline.addCameraShot(new CameraShot(shot.beginCount[0],
-                    shot.endCount[0], shot.name[0], shot.description[0]));
-            });
-            const cameraTimeline2 = new CameraTimeline("dummyName2", "dummyDescription2");
-            secondTimelineXML.shot.forEach(shot => {
-                cameraTimeline2.addCameraShot(new CameraShot(shot.beginCount[0],
-                    shot.endCount[0], shot.name[0], shot.description[0]));
-            });
 
             const cameraTimelines = new Array();
-            cameraTimelines.push(cameraTimeline);
-            cameraTimelines.push(cameraTimeline2);
+            const flattenedCameraTimelines = new Array();
+
+            cameraTimelinesXML.forEach(timeline => {
+                console.log(timeline);
+                const cameraTimeline = new CameraTimeline("dummy", "dymmy");
+                if (typeof timeline.shotList[0].shot !== "undefined") {
+                    timeline.shotList[0].shot.forEach(shot => {
+                        const cameraShot = new CameraShot(shot.beginCount[0], shot.endCount[0], shot.name[0], shot.description[0]);
+                        cameraTimeline.addCameraShot(cameraShot);
+                        flattenedCameraTimelines.push(cameraShot);
+                    });
+                } else {
+                    console.log("undefined bitch")
+                }
+
+                cameraTimelines.push(cameraTimeline);
+            });
 
             // Calculate minimum and maximum counts
             let minCount = 0;
             let maxCount = 0;
 
-            const flattenedTimeline = new Array();
-            cameraTimeline.getCameraShots().forEach(shot => {
-                flattenedTimeline.push(shot);
-            });
-            cameraTimeline2.getCameraShots().forEach(shot => {
-                flattenedTimeline.push(shot);
-            })
-
-            if (cameraTimeline.getCameraShots().length > 0) {
-                minCount = Number(cameraTimeline.getCameraShots()[0].beginCount);
-                flattenedTimeline.forEach(shot => {
-                    console.log(shot.beginCount);
-                    console.log(minCount);
+            if (flattenedCameraTimelines.length > 0) {
+                minCount = Number(flattenedCameraTimelines[0].beginCount);
+                maxCount = Number(flattenedCameraTimelines[0].endCount);
+                flattenedCameraTimelines.forEach(shot => {
                     if (shot.beginCount < minCount) {
                         minCount = Number(shot.beginCount);
-                        console.log(minCount);
                     }
-                });
-                maxCount = Number(cameraTimeline.getCameraShots()[0].endCount);
-                flattenedTimeline.forEach(shot => {
                     if (shot.endCount > maxCount) {
                         maxCount = Number(shot.endCount);
                     }
