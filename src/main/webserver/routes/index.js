692cd01cc5c9c194be4f7bea6d0786c04d174c2f
Bart de Jonge <B.A.P.deJonge@student.tudelft.nl>
2016-05-23 12:08:34 +0200

    moved webserver to new repository

--- src/main/webserver/routes/index.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-const router = express.Router(); // eslint-disable-line new-cap
-
-/* GET home page. */
-router.get("/", (req, res) => {
-    res.render("index");
-});
-
-module.exports = router;
