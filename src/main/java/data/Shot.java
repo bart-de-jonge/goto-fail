e38eead3f4182b9ea935243391205da748913c13
Bart de Jonge <B.A.P.deJonge@student.tudelft.nl>
2016-06-15 10:50:52 +0200

    fixed failing build

--- src/main/java/data/Shot.java
+++ src/main/java/data/Shot.java
@@ -6,7 +6,12 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.log4j.Log4j2;
 
-import javax.xml.bind.annotation.*;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementWrapper;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlTransient;
 import java.util.ArrayList;
 
 /**
