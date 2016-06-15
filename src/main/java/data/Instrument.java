b59c9cb276ac07b90d25482fcbbf3c4aee1f4524
Bart de Jonge <B.A.P.deJonge@student.tudelft.nl>
2016-06-15 10:36:42 +0200

    improved instruments branch coverage

--- src/main/java/data/Instrument.java
+++ src/main/java/data/Instrument.java
@@ -1,10 +1,8 @@
 package data;
 
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-@EqualsAndHashCode
 public class Instrument implements Cloneable {
     @Getter @Setter
     private String name;
@@ -39,4 +37,15 @@ public class Instrument implements Cloneable {
         return instrument;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Instrument that = (Instrument) o;
+
+        if (getName() != null ? !getName().equals(that.getName()) : that.getName() != null) return false;
+        return getDescription() != null ? getDescription().equals(that.getDescription()) : that.getDescription() == null;
+
+    }
 }
